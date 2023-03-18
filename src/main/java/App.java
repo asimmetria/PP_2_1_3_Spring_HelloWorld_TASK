422542f16b1d459b8d6c571bcf3e024661cd0a52
Mikhail Altunin <altunin@list.ru>
2023-03-19 02:51:24 +0300

    solution

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -5,8 +5,15 @@ public class App {
     public static void main(String[] args) {
         ApplicationContext applicationContext =
                 new AnnotationConfigApplicationContext(AppConfig.class);
-        HelloWorld bean =
-                (HelloWorld) applicationContext.getBean("helloworld");
+        HelloWorld bean = (HelloWorld) applicationContext.getBean("helloworld");
         System.out.println(bean.getMessage());
+        HelloWorld bean1 = (HelloWorld) applicationContext.getBean("helloworld");
+
+        Cat cat = applicationContext.getBean("cat", Cat.class);
+        Cat cat1 = applicationContext.getBean("cat", Cat.class);
+
+        System.out.println(bean == bean1);
+        System.out.println(cat == cat1);
+
     }
 }
--- src/main/java/AppConfig.java
+++ src/main/java/AppConfig.java
@@ -2,6 +2,8 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Scope;
 
+import java.util.Random;
+
 @Configuration
 public class AppConfig {
  
@@ -10,5 +12,13 @@ public class AppConfig {
         HelloWorld helloWorld = new HelloWorld();
         helloWorld.setMessage("Hello World!");
         return helloWorld;
+    }
+    @Bean(name = "cat")
+    @Scope("prototype")
+    public Cat getCat(){
+        Cat cat = new Cat();
+        Random rand = new Random();
+        cat.setId(rand.nextInt(100));
+        return cat;
     }
 }
